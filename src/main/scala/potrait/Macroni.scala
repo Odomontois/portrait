a25b4a023c09c8c7186a1e8e6f815a6ec12b5b36
Oleg Nizhnik <odomontois@gmail.com>
2022-06-22 23:49:07 +0400

    @@@@

--- src/main/scala/potrait/Macroni.scala
+++ src/main/scala/potrait/Macroni.scala
@@ -1,5 +1,7 @@
 package potrait
 import scala.quoted.*
+import scala.annotation.experimental
+import scala.language.reflectiveCalls
 
 object Macroni:
   transparent inline def lol[T <: AnyKind]: String = ${ lolMacro[T] }
@@ -14,25 +16,66 @@ class Macroni[T <: AnyKind: Type](using q: Quotes):
 
   def lolli: Expr[String] = Expr(lolRepr(TypeRepr.of[T]))
 
+  private def checkApps(params: List[String], apps: List[TypeRepr]): Boolean =
+    apps.collect { case ParamRef(_, i) => i } == params.indices
+
   private def lolRepr(x: TypeRepr): String =
     (x.dealias.widen, x.classSymbol) match
       case (TypeRef(t, name), Some(sym)) =>
-        s"""TypeRef $t $name 
-             |
-             |${lolSymbol(sym)}""".stripMargin
+        s"""=========================
+           |TypeRef $t $name
+           |------------------------ 
+           |
+           |${lolSymbol(sym)}
+           |========================""".stripMargin
 
-      case (TypeLambda(params, bounds, AppliedType(inner, apps)), _) =>
-        val recur = lolRepr(inner)
-
-        s"""TypeLambda $params $apps
+      case (TypeLambda(params, _, AppliedType(inner, apps)), _) if checkApps(params, apps) =>
+        s"""TypeLambda $params
              |
-             |$recur""".stripMargin
+             |${lolRepr(inner)}""".stripMargin
 
       case _ =>
         s"Macroni $x"
 
   private def lolSymbol(s: Symbol): String =
     s"""
-       | ${s.isClassDef}
-       | ${s.declarations}
+       | class def: ${s.isClassDef}
+       | declarations
+       |------------------------
+       |${s.declarations.map(lolDeclaration).mkString("\n")}
+       |------------------------
        |""".stripMargin
+
+  private def lolDeclaration(s: Symbol): String =
+    if s.isType
+    then s" TYPE $s"
+    else if s.isDefDef && s.name != "<init>"
+    then lolDefDecl(s)
+    else if s.isValDef
+    then lolValDecl(s)
+    else s.toString
+
+  private def lolDefDecl(s: Symbol): String =
+    val sign = s.signature
+    val Signature(params, res) = sign
+    s""" DEF $s ${s.flags}
+       | SIGNATURE: $sign
+       | PARAMS ${params.mkString(",")}
+       | RESULT $res
+       | FLAGS: ${getFlags(s.flags).mkString(",")}
+       |""".stripMargin
+
+  private def lolValDecl(s: Symbol): String =
+    s""" VAL $s ${s.flags}
+       | FLAGS: ${getFlags(s.flags).mkString(",")}
+       |""".stripMargin
+
+  private val allFlags =
+    Flags.getClass.getDeclaredMethods.iterator
+      .filter(_.getReturnType == classOf[Long])
+      .map(f => f.getName -> f.invoke(Flags).asInstanceOf[Flags])
+      .filter(_._2 != 0)
+      .toMap
+
+  private def getFlags(s: Flags) =
+    for (name, flag) <- allFlags if s.is(flag) yield name
--- src/test/scala/macroni/MacroniTest.scala
+++ src/test/scala/macroni/MacroniTest.scala
@@ -1,17 +1,24 @@
 package macroni
 
 import potrait.Macroni
+import scala.annotation.experimental
+import scala.annotation.nowarn
 
 trait Foo:
   type X
+  val z: Int = 2
   def a(s: String): Int
   def b(x: X)(z: List[X]): X
+  def b1(x: X): X = b(x)(Nil)
+  final def b2(x: X): X = b(x)(List(x))
 
 trait Bar[A[_]]:
   def u(s: String): Int
   def b(uu: List[Int]): A[String]
 
-@main def MacroniTes() =
+@nowarn
+@main def MacroniTest() =
+
   println(Macroni.lol[Foo])
 
   println(Macroni.lol[Bar])
